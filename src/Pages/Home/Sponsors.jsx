9d8b7de640490483070c4c32d5704c3fb1958c23
Abu Taher <writerabutaher@gmail.com>
2023-06-02 09:07:20 +0600

    sponsors

--- src/Pages/Home/Sponsors.jsx
+++ src/Pages/Home/Sponsors.jsx
@@ -7,7 +7,7 @@ const Sponsors = () => {
             <div className="relative flex flex-col items-center overflow-hidden py-16 md:py-20 my-20 bg-gray-100 ">
                 <div className="container relative z-[1] m-auto px-6 md:px-12">
                     <div className="m-auto text-center md:w-8/12 lg:w-6/12">
-                        <h2 className="text-2xl text-gray-700 font-bold md:text-4xl">Your favorite companies are our partners.</h2>
+                        <h1 className="text-4xl font-semibold text-center my-8">Favorite Companies Our Partners.</h1>
                     </div>
                     <div className="m-auto mt-16 md:w-11/12">
                         <div className="flex flex-wrap justify-center gap-6">
